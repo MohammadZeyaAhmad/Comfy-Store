49438cdca32abbc3cc8a536e87420f14f7fdeeb4
mohammad zeya ahmad <mohammadzeyaahmad@gmail.com>
2023-11-20 11:29:53 +0530

    featured products added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {About,Cart,Checkout,Error,HomeLayout,Login,Register,Products,SingleProduct,Orders,Landing} from './pages'
 import {RouterProvider,createBrowserRouter} from 'react-router-dom'
+import { ErrorElement } from './components';
+import { loader as landingLoader } from './pages/Landing';
+import {loader as SingleProductLoader } from './pages/SingleProduct';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -10,6 +13,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        loader: landingLoader,
+        errorElement:ErrorElement
       },
       {
         path: 'products',
@@ -18,6 +23,8 @@ const router = createBrowserRouter([
       {
         path: 'products/:id',
         element: <SingleProduct />,
+        errorElement:ErrorElement,
+        loader: SingleProductLoader
       },
       {
         path: 'cart',
