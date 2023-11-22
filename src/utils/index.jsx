6dc373ce07a904702bb95e85ad6ff1316973d1d3
Mohammad Zeya Ahmad <62665238+MohammadZeyaAhmad@users.noreply.github.com>
2023-11-22 16:37:53 +0530

    Update index.jsx

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const productionUrl = "https://strapi-store-server.onrender.com/api";
+const productionUrl = "https://strapi-store-server.onrender.com/api";
 
 export const customFetch = axios.create({
-  baseURL: `${process.env.VITE_SECRET}`,
+  baseURL: productionUrl,
 });
 
 export const formatPrice = (price) => {
